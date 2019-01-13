95beadc8077c37880b182f8fc5ada76790ce627b
caixuanbin <caixuanbin_1988@126.com>
2019-01-13 12:21:05 +0800

    实现一个springboot+springsecurity+springjpa 的登录验证

--- myweb/src/main/java/com/xbcai/myweb/controller/SecurityControlller.java
+++ myweb/src/main/java/com/xbcai/myweb/controller/SecurityControlller.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityControlller {
-
     @GetMapping("/index")
     public String index(){
         return "index";
